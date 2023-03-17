3c1e8b1062c60e8fa5691e13c25b156d77dc0b53
TobyBritton1 <Toby@lumilinks.com>
2023-03-17 16:36:33 +0000

    work on dbscan

--- Clustering.py
+++ Clustering.py
@@ -1,10 +1,7 @@
 from sklearn.cluster import DBSCAN
 
-max_dist = 0.5
-min_samples = 2
-
 # Uses euclidean distance so all need to be across the same range
-def ClusterMetrics(metrics):
+def ClusterMetrics(metrics, max_dist=0.5, min_samples=2):
     clustering = DBSCAN(eps=max_dist, min_samples=min_samples).fit(metrics)
     labels = clustering.labels_
     return labels
--- Metrics.py
+++ Metrics.py
@@ -8,6 +8,11 @@ def getSentiment(text):
 def getFactBased(text):
     text = text.replace('“','"').replace('”','"')
     quotes = text.count('"')
-    length = len(text.split(' '))
-    factBased = round(quotes/length,5)
-    return factBased
+    length = len(text) + 1 # Laplace smoothing
+    factBased = quotes / length
+    factBased = (factBased / 0.0075) - 1
+    if factBased > 1:
+        factBased = 1
+    elif factBased < -1:
+        factBased = -1
+    return round(factBased,5), quotes, length
