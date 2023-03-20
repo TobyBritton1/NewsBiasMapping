a201624a9d29e9e83dc7893d652d0c8deaf76f95
TobyBritton1 <Toby@lumilinks.com>
2023-03-20 10:31:30 +0000

    started sensationalized metric

--- Metrics.py
+++ Metrics.py
@@ -16,3 +16,62 @@ def getFactBased(text):
     elif factBased < -1:
         factBased = -1
     return round(factBased,5), quotes, length
+
+def getSensationalized(text):
+    sensationalizedLanguage = [
+        'shock',
+        'terrify',
+        'explosive',
+        'epic',
+        'mind-blowing',
+        'unbelievable',
+        'stunning',
+        'scandalous',
+        'sensational',
+        'incredible',
+        'jaw-dropping',
+        'unprecedented',
+        'apocalypse',
+        'catastrophic',
+        'devastating',
+        'heartbreaking',
+        'horrific',
+        'intense',
+        'massive',
+        'monstrous',
+        'phenomenal',
+        'record-breaking',
+        'shock-and-awe',
+        'spectacular',
+        'traumatic',
+        'ultimate',
+        'unimaginable',
+        'apocalyptic',
+        'bombshell',
+        'brutal',
+        'chaotic',
+        'deadly',
+        'doom',
+        'eerie',
+        'frenzy',
+        'gripping',
+        'insane',
+        'killer',
+        'lethal',
+        'meltdown',
+        'nightmarish',
+        'pandemonium',
+        'rampage',
+        'savage',
+        'tragic',
+        'unhinged',
+        'vicious',
+        'widespread',
+        'x-treme',
+        'zany'
+    ]
+    text = text.lower()
+    sensationalizedCount = 0
+    for word in sensationalizedLanguage:
+        sensationalizedCount += text.count(word)
+    return sensationalizedCount
