13b32fb230a03315eb128130fbe5fc0d06b21be5
TobyBritton1 <Toby@lumilinks.com>
2023-05-01 15:38:56 +0100

    cleaning up

--- NewsBiasClustering.py
+++ NewsBiasClustering.py
@@ -8,8 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from Metrics import getSentiment, getQuoteBased, getSensationalized, getMudslinging, getSpin, getInformal
 from Clustering import BirchCluster
 
-
-def NewsBiasClustering(articles, threshold=0.32, n_clusters=None):    
+def getMetrics(articles):
     # Get Metrics
     metrics = []
     for i in range(len(articles)):
@@ -20,48 +19,33 @@ def NewsBiasClustering(articles, threshold=0.32, n_clusters=None):
         spin = getSpin(articles.at[i,'body'])
         informal = getInformal(articles.at[i,'body'])
         metrics.append([sentiment, quoteBased, sensationalized, mudslinging, spin, informal])
-    
-    # Cluster metrics
-    labels = BirchCluster(metrics, threshold=threshold, n_clusters=n_clusters)
-    print(Counter(labels))
+    return metrics
 
-    # Reduce Data Dimensions
-    data = pd.DataFrame(metrics,columns=['sentiment','quoteBased','sensationalized','mudslinging','spin','informal'])
-    scalar = StandardScaler()
-    scaledData = pd.DataFrame(scalar.fit_transform(data),columns=['sentiment','quoteBased','sensationalized','mudslinging','spin','informal']) #scaling the data
-    #sns.heatmap(scaledData.corr())
-    pca = PCA(n_components = 2)
-    pca.fit(scaledData)
-    dataPCA = pca.transform(scaledData)
-    dataPCA = pd.DataFrame(dataPCA,columns=['PC1','PC2'])
-    #sns.heatmap(dataPCA.corr())
-
-    # Output Clustered Graph
-    for i in range(len(Counter(labels))):
-        pc1 = []
-        pc2 = []
-        for j in range(len(labels)):
-            if labels[j] == i:
-                pc1.append(dataPCA.iloc[j]['PC1'])
-                pc2.append(dataPCA.iloc[j]['PC2'])
-        plt.scatter(pc1,pc2,s=5)
-    plt.show()
-
+def getWord(value):
+    if value < 0.2:
+        return 'very low '
+    elif value < 0.4:
+        return 'low '
+    elif value < 0.6:
+        return 'medium '
+    elif value < 0.8:
+        return 'high '
+    else:
+        return 'very high '
 
 def NewsBiasClusteringFromMetrics(metrics, threshold=0.32, n_clusters=None):
     # Reduce Data Dimensions
     data = pd.DataFrame(metrics,columns=['sentiment','quoteBased','sensationalized','mudslinging','spin','informal'])
     scalar = StandardScaler()
     scaledData = pd.DataFrame(scalar.fit_transform(data),columns=['sentiment','quoteBased','sensationalized','mudslinging','spin','informal']) #scaling the data
-    #sns.heatmap(scaledData.corr())
-    #plt.show()
+    # sns.heatmap(scaledData.corr())
+    # plt.show()
     pca = PCA(n_components = 2)
     pca.fit(scaledData)
     dataPCA = pca.transform(scaledData)
     dataPCA = pd.DataFrame(dataPCA,columns=['PC1','PC2'])
-    #sns.heatmap(dataPCA.corr())
-    #plt.show()
-
+    # sns.heatmap(dataPCA.corr())
+    # plt.show()
 
     # Cluster metrics
     labels = BirchCluster(metrics, threshold=threshold, n_clusters=n_clusters)
@@ -70,9 +54,7 @@ def NewsBiasClusteringFromMetrics(metrics, threshold=0.32, n_clusters=None):
     # Get Dominant Metrics For Each Label
     data = pd.DataFrame(metrics,columns=['sentiment','quoteBased','sensationalized','mudslinging','spin','informal'])
     data['label'] = labels
-    
     means = data.groupby(['label']).mean()
-
     dominantMetrics = []
     dominantMetricsWords = []
     for i in range(len(Counter(labels))):
@@ -104,7 +86,6 @@ def NewsBiasClusteringFromMetrics(metrics, threshold=0.32, n_clusters=None):
     dominantMetricsDataFrame = pd.DataFrame([i for i in range(len(Counter(labels)))], columns=['label'])
     dominantMetricsDataFrame['dominantMetrics'] = dominantMetrics
     dominantMetricsDataFrame['dominantMetricsWords'] = dominantMetricsWords
-    print(dominantMetricsWords)
 
     # Output Clustered Graph
     for i in range(len(Counter(labels))):
@@ -121,15 +102,6 @@ def NewsBiasClusteringFromMetrics(metrics, threshold=0.32, n_clusters=None):
     plt.show()
     return labels, dataPCA, dominantMetricsDataFrame
 
-
-def getWord(value):
-    if value < 0.2:
-        return 'very low '
-    elif value < 0.4:
-        return 'low '
-    elif value < 0.6:
-        return 'medium '
-    elif value < 0.8:
-        return 'high '
-    else:
-        return 'very high '
+def NewsBiasClustering(articles, threshold=0.32, n_clusters=None):    
+    metrics = getMetrics(articles)
+    return NewsBiasClusteringFromMetrics(metrics, threshold, n_clusters)
